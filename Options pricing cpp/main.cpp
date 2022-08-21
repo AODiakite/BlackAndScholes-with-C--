9276ad68bf6795477b2cc4602a72b26b00c1755a
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2022-08-21 11:53:18 +0100

    Initial Commit

---
+++ Options pricing cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Options pricing cpp
+//
+//  Created by Abdoul Oudouss Diakite on 21/08/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
